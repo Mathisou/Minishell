a0384fa6a606174c30a540f332d89e8cb1ae401a
Hugo KOVAC <hkovac@e2r5p17.clusters.42paris.fr>
2022-02-24 16:41:14 +0100

    split with ><

--- sources/utils/split2.c
+++ sources/utils/split2.c
@@ -3,16 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   split2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maroly <maroly@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hkovac <hkovac@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 13:58:29 by hkovac            #+#    #+#             */
-/*   Updated: 2022/02/21 17:16:25 by maroly           ###   ########.fr       */
+/*   Updated: 2022/02/24 16:40:37 by hkovac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static int	create_tab(char const *s, char c)
+void	test62(int *i, int *size, char const *s)/* test les < > dpour split les char * */
+{
+	if (s[*i] == 62 || (s[*i] == 62 && s[(*i) + 1] == 62))
+	{
+		if ((*i) != 0 && s[(*i) - 1] != ' ')
+			(*size)++;
+		if (s[(*i) + 1] == 62)
+			(*i)++;
+		if (s[(*i) + 1] && s[(*i) + 1] != ' ')
+			(*size)++;
+	}
+}
+
+void	test60(int *i, int *size, char const *s)
+{
+	if (s[*i] == 60 || (s[*i] == 60 && s[(*i) + 1] == 60))
+	{
+		if ((*i) != 0 && s[(*i) - 1] != ' ')
+			(*size)++;
+		if (s[(*i) + 1] == 60)
+			(*i)++;
+		if (s[(*i) + 1] && s[(*i) + 1] != ' ')
+			(*size)++;
+	}
+}
+
+static int	create_tab(char const *s, char c)/* compte cmb de char * dans char ** */
 {
 	int	i;
 	int	size;
@@ -31,37 +57,56 @@ static int	create_tab(char const *s, char c)
 					;
 			size++;
 		}
+		test62(&i, &size, s);
+		test60(&i, &size, s);
 		if (s[i])
 			i++;
 	}
+	printf("=> %d\n", size);
 	return (size);
 }
 
-char	*count_mall(const char *s, char c)
+char	*count_mall(const char *s, char c)/* compte combien mall dans char * */
 {
 	size_t	i;
 	char	*str;
 
 	str = NULL;
-	i = 0;
-	while (s[i] != c && s[i])
+	i = 0;		
+	if (s[i] == 62)
 	{
-		if (s[i] == 39)
-			while (s[++i] != 39)
-				;
-		else if (s[i] == 34)
-			while (s[++i] != 34)
-				;
-		if (s[i])
+		i++;
+		if (s[i] == 62)
 			i++;
 	}
+	else if (s[i] == 60)
+	{
+		i++;
+		if (s[i] == 60)
+			i++;
+	}
+	else 
+	{	
+		while (s[i] != c && s[i] && s[i] != 60 && s[i] != 62)
+		{
+			if (s[i] == 39)
+				while (s[++i] != 39)
+					;
+			else if (s[i] == 34)
+				while (s[++i] != 34)
+					;
+			if (s[i])
+				i++;
+		}
+	}
+	printf("str = %li\n", i);
 	str = (char *) malloc(sizeof(*str) * (i + 1));
 	if (!str)
 		return (NULL);
 	return (str);
 }
 
-void	put_str_in_tab2(char *str, int *i, const char *s)
+void	put_str_in_tab2(char *str, int *i, const char *s)/*extension de en bas*/
 {
 	str[*i] = s[*i];
 	if (s[*i] == 39)
@@ -80,7 +125,7 @@ void	put_str_in_tab2(char *str, int *i, const char *s)
 		(*i)++;
 }
 
-static char	*put_str_in_tab(const char *s, char c)
+static char	*put_str_in_tab(const char *s, char c)/*copy de sep a sep*/
 {
 	int	i;
 	char	*str;
@@ -89,15 +134,43 @@ static char	*put_str_in_tab(const char *s, char c)
 	if (str == NULL)
 		return (NULL);
 	i = 0;
-	while (s[i] && s[i] != c)
-		put_str_in_tab2(str, &i, s);
+	if (s[i] == 60)
+	{
+		str[i] = s[i];
+		i++;
+		if (s[i] == 60)
+		{
+			str[i] = s[i];
+			i++;
+		}
+	}
+	else if (s[i] == 62)
+	{
+		str[i] = s[i];
+		i++;
+		if (s[i] == 62)
+		{
+			str[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (s[i] && s[i] != c && s[i] != 60 && s[i] != 62)
+			put_str_in_tab2(str, &i, s);
+	}
 	str[i] = '\0';
 	return (str);
 }
 
-char **norm1(t_norm *norm, char **big_tab)
+char **norm1(t_norm *norm, char **big_tab)/*parcours tout str * et dispatch*/
 {
-	if (((norm->i == 0 || norm->s[norm->i - 1] == norm->c) && norm->s[norm->i] != norm->c) && norm->s[norm->i])
+	if (((norm->i == 0 || norm->s[norm->i - 1] == norm->c)/*sep before*/
+	&& norm->s[norm->i] != norm->c) && norm->s[norm->i]/*current sep*/
+	|| norm->s[norm->i] == 60 && norm->s[norm->i - 1] && norm->s[norm->i - 1] != ' ' && norm->s[norm->i - 1] != 60
+	|| norm->s[norm->i] == 62 && norm->s[norm->i - 1] && norm->s[norm->i - 1] != ' ' && norm->s[norm->i - 1] != 62
+	|| norm->s[norm->i - 1] == 60 && norm->s[norm->i] != 60
+	|| norm->s[norm->i - 1] == 62 && norm->s[norm->i] != 62)
 	{
 		big_tab[norm->string] = put_str_in_tab(&norm->s[norm->i], norm->c);
 		if (!big_tab[norm->string])
@@ -130,11 +203,24 @@ char	**split2(char const *s, char c)
 	if (big_tab == NULL)
 		return (NULL);
 	while (s[norm.i])
-	{
-		rtn = norm1(&norm, big_tab);
-		if (rtn)
-			return (rtn);
-	}
+		norm1(&norm, big_tab);
 	big_tab[norm.string] = NULL;
 	return (big_tab);
+}
+
+int main(int ac, char **av)
+{
+	char **t;
+
+	if (ac == 2)
+	{
+		t = split2(av[1], ' ');
+		for (int i = 0; t[i]; i++)
+		{
+			printf("%s\n", t[i]);
+			free(t[i]);
+		}
+		free(t);
+	}
+	return (0);	
 }
