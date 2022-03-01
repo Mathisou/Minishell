73f4b168841c0d9c307566e78ef1d957636bb21b
Hugo KOVAC <hkovac@e3r12p4.clusters.42paris.fr>
2022-03-01 14:47:25 +0100

    env full normed

--- sources/env/own_env.c
+++ sources/env/own_env.c
@@ -3,55 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   own_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maroly <maroly@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hkovac <hkovac@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 14:15:49 by hkovac            #+#    #+#             */
-/*   Updated: 2022/02/26 13:31:21 by maroly           ###   ########.fr       */
+/*   Updated: 2022/03/01 14:46:07 by hkovac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	take_env(char   **env, t_env **envi)
+void	take_env2(char **env, t_env **envi, t_takee *nrm)
 {
-	int		i;
-	char	*new_shlvl;
-	char 	*rtn_itoa;
-	int		exist[2];
-
-	i = -1;
-	new_shlvl = NULL;
-	exist[0] = 0;
-	exist[1] = 0;
-	while (env[++i])
+	while (env[++nrm->i])
 	{
-		if (ft_strncmp(env[i], "PWD=", 4) == 0)
-			exist[1]++;
-		if (ft_strncmp(env[i], "SHLVL=", 6) == 0)
+		if (ft_strncmp(env[nrm->i], "PWD=", 4) == 0)
+			nrm->exist[1]++;
+		if (ft_strncmp(env[nrm->i], "SHLVL=", 6) == 0)
 		{
-			rtn_itoa = ft_itoa(ft_atoi(&env[i][6]) + 1);
-			new_shlvl = strcats("SHLVL=", rtn_itoa);
-			add_node_back(envi, new_shlvl);
-			free(new_shlvl);
-			free(rtn_itoa);
-			exist[0]++;
+			nrm->rtn_itoa = ft_itoa(ft_atoi(&env[nrm->i][6]) + 1);
+			nrm->new_shlvl = strcats("SHLVL=", nrm->rtn_itoa);
+			add_node_back(envi, nrm->new_shlvl);
+			free(nrm->new_shlvl);
+			free(nrm->rtn_itoa);
+			nrm->exist[0]++;
 		}
 		else
-			add_node_back(envi, env[i]);
-	}
-	if (!exist[0])
-		add_node_back(envi, "SHLVL=1");
-	if (!exist[1])
-	{
-		new_shlvl = getcwd(NULL, 4096 + 1);
-		rtn_itoa = strcats("PWD=", new_shlvl);
-		add_node_back(envi, rtn_itoa);
-		free(new_shlvl);
-		free(rtn_itoa);
+			add_node_back(envi, env[nrm->i]);
 	}
 }
 
-char **convert_env(t_env **lst)
+void	take_env(char **env, t_env **envi)
+{
+	t_takee	nrm;
+
+	nrm.i = -1;
+	nrm.new_shlvl = NULL;
+	nrm.exist[0] = 0;
+	nrm.exist[1] = 0;
+	take_env2(env, envi, &nrm);
+	if (!nrm.exist[0])
+		add_node_back(envi, "SHLVL=1");
+	if (!nrm.exist[1])
+	{
+		nrm.new_shlvl = getcwd(NULL, 4096 + 1);
+		nrm.rtn_itoa = strcats("PWD=", nrm.new_shlvl);
+		add_node_back(envi, nrm.rtn_itoa);
+		free(nrm.new_shlvl);
+		free(nrm.rtn_itoa);
+	}
+}
+
+char	**convert_env(t_env **lst)
 {
 	t_env	*tmp;
 	char	**big;
@@ -62,10 +64,10 @@ char **convert_env(t_env **lst)
 	tmp = *lst;
 	size = lst_size(lst);
 	if (size == 0)
-		return (NULL); //a securiser
+		return (NULL);
 	big = malloc(sizeof(*big) * (size + 1));
 	if (!big)
-		return (NULL); // a securiser
+		return (NULL);
 	while (tmp)
 	{
 		big[i] = ft_strdup(tmp->var);
