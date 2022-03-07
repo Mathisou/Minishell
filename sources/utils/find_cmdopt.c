eea71a1a36635d7ff990ac1b200d57c7f737ae3f
Mathisou <mathisrol02@gmail.com>
2022-03-07 01:12:06 +0100

    fixed cmdopt (|| --> && ) todo : fix split (spaces)

--- sources/utils/find_cmdopt.c
+++ sources/utils/find_cmdopt.c
@@ -6,7 +6,7 @@
 /*   By: maroly <maroly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 14:04:11 by maroly            #+#    #+#             */
-/*   Updated: 2022/03/04 17:50:53 by maroly           ###   ########.fr       */
+/*   Updated: 2022/03/07 01:03:21 by maroly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static char	***find_opt2(char **bt, t_opt *nrm, t_global *global)
 
 	start = 0;
 	while (bt[nrm->j + start] && (check_limiter(bt[nrm->j + start]) == 0
-			|| ft_strcmp(bt[nrm->j + start], "|") != 0))
+			&& ft_strcmp(bt[nrm->j + start], "|") != 0))
 		start++;
 	nrm->cmdopt[nrm->k] = malloc(sizeof(char *) * (start + 1));
 	if (!nrm->cmdopt[nrm->k])
